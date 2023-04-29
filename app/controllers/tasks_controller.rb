a6b8b84cdb6f2f986a276765d47160265b4239fd
amaury dav <amauryat@e-nautia.com>
2023-04-29 16:57:43 +0200

    partial

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
+end
